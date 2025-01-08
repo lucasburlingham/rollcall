175c7fdb3be44d471622bd192279b169bab449bc
Lucas Burlingham <lucas@lucasburlingham.me>
2025-01-07 22:32:51 -0500

    Let's set the rust aside...

--- server/src/main.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, sqlx::FromRow)]
-struct User {
-    id: i32,
-    username: String,
-    email: String,
-}
-
-let pool = sqlx::sqlite::SqlitePool::connect("sqlite:messages.db").await?;
-
